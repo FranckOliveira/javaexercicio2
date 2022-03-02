bd2ec2d341744696f479d8f06d3bdfd9eb12fcbe
Francisco Gleriston da Oliveira <88321125+FranckOliveira@users.noreply.github.com>
2022-03-02 17:55:56 -0300

    Update Triangulo.java
    Inserindo o cálculo da área do triangulo na classe triangulo.

--- src/entidades/Triangulo.java
+++ src/entidades/Triangulo.java
@@ -5,10 +5,10 @@ public class Triangulo {
 	public double a;
 	public double b;
 	public double c;
-	public static void main (String[] args) {
-		
-		
-		
+	
+	public double area() {
+		double p = (a+b+c)/2.0;
+		return Math.sqrt(p*(p-a)*(p-b)*(p-c));
 	}
 
 }
